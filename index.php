37649cb12856a5bd7c5bb7157dee44528b89d0d7
Alan Lin <alanlin@Alans-MacBook-Pro.local>
2017-01-17 23:27:56 -0800

    added homepage styling

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/sidebarnav.css">
 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
@@ -17,7 +18,50 @@
   </head>
   <body>
 
-    
+    <div class="nav-side-menu">
+        <div class="brand"><a href="index.php"><img src="img/logo.svg" alt=""></a></div>
+        <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+
+            <div class="menu-list">
+
+                <ul id="menu-content" class="menu-content collapse out">
+                    <li class="active">
+                      <a href="#">Work</a>
+                    </li>
+                     <li>
+                      <a href="#">Profile</a>
+                      </li>
+                      <li>
+                       <a href="#">Cart</a>
+                     </li>
+                     <li>
+                      <a href="#">Login</a>
+                    </li>
+                    <li>
+                     <a href="#">Logout</a>
+                   </li>
+                </ul>
+         </div>
+    </div>
+
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="https://placekitten.com/g/250/250" alt="">
+          </div>
+          <div class="col-md-6">
+            <img src="https://placekitten.com/g/250/250" alt="">
+          </div>
+          <div class="col-md-6">
+            <img src="https://placekitten.com/g/250/250" alt="">
+          </div>
+          <div class="col-md-6">
+            <img src="https://placekitten.com/g/250/250" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
